cda75e21497fb914e588d6b11ed183ac66a2f3bd
Thais Oliveira <thais_oliv_@outlook.com>
2025-03-18 09:09:28 -0300

    exercícios web

--- Lista 2/JS/js.js
+++ Lista 2/JS/js.js
@@ -1,3 +1,7 @@
+function seguir(){
+    alert('Você agora está seguindo Thais')
+}
+
 function exe1(){
     let nota1 = Number(document.getElementById("nota1").value)
     let nota2 = Number(document.getElementById("nota2").value)
@@ -38,7 +42,56 @@ function exe3(){
         document.getElementById("menor").innerText = "O menor número é: " + menor
     }
     else{
-        nro2 = menor
-        document.getElementById("menor").innerText = "O menor número é: " + menor
     }
+    }
+
+function exe4(){
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let nro3 = Number(document.getElementById("nro3").value)
+    if (nro1 >= nro2 && nro1 >= nro3){
+        maior = nro1
+        document.getElementById("maior").innerText = "O maior número é: " + maior
+    }
+    else if (nro2 >= nro1 && nro2 >= nro3){
+        maior = nro2
+        document.getElementById("maior").innerText = "O maior número é: " + maior
+    }
+    else if (nro3 >= nro1 && nro3 >= nro2){
+        maior = nro3
+        document.getElementById("maior").innerText = "O maior número é: " + maior
+    }
+    else{
+        document.getElementById("maior").innerText = "Os números são iguais"
+    }
+}
+
+function exe5(){
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    let selecao = Number(document.getElementById("selecao").value)
+
+    switch (selecao){
+        case 1: document.getElementById("resultado").innerText = (nro1 + nro2) / 2
+        break
+        
+        case 2: if (nro1 >= nro2){
+            document.getElementById("resultado").innerText = nro1 - nro2
+        }
+        else {
+            document.getElementById("resultado").innerText = nro2 - nro1
+        }
+        break
+
+        case 3: document.getElementById("resultado").innerText = nro1 * nro2
+        break
+
+        case 4: if (nro2 != 0){
+            document.getElementById("resultado").innerText = nro1 / nro2
+        }
+        else {
+            document.getElementById("resultado").innerText = "Divisão por 0 inválida!"
+        }
+    }
+
 }
